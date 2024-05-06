84d40064c332ab5e7bcc15014815e0f463726b7e
Rustam <easton1234537@gmail.com>
2024-05-06 17:22:45 +0300

    first

---
+++ src/main/java/ru/rstd/mygoods/MyGoodsApplication.java
@@ -0,0 +1,13 @@
+package ru.rstd.mygoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyGoodsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/rstd/mygoods/MyGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rstd.mygoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
